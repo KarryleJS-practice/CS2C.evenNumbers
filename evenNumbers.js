69a928d5092f91548ce8b73920dbd0298b74be46
KarryleJS-practice <karrylelagrimas6@gmail.com>
2024-10-03 22:47:12 +0800

    evenNumbers.js

---
+++ evenNumbers.js
@@ -0,0 +1,9 @@
+// evenNumbers.js
+
+// For loop to print even numbers between 1 and 10
+for (let i = 1; i <= 10; i++) {
+    // Check if the number is even
+    if (i % 2 === 0) {
+        console.log(i); // Log the even number to the console
+    }
+}
